aa9818f5ff0b582197c0c88639fe7d312d6a5c9d
zwd3413063 <git>
2015-05-19 20:55:16 +0800

    123

---
+++ GitTest/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  GitTest
+//
+//  Created by 钟伟迪 on 15/5/19.
+//  Copyright (c) 2015年 钟伟迪. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+@end
---
+++ GitTest/Test1.m
@@ -0,0 +1,13 @@
+//
+//  Test1.m
+//  GitTest
+//
+//  Created by 钟伟迪 on 15/5/19.
+//  Copyright (c) 2015年 钟伟迪. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+@end
